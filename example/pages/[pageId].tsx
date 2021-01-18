e60b74af115820ac4f9fd4598fd3f97e911c3c9b
Travis Fischer <fisch0920@gmail.com>
2021-01-18 18:37:05 -0500

    minor tings

--- example/pages/[pageId].tsx
+++ example/pages/[pageId].tsx
@@ -5,6 +5,8 @@ import { getPageTitle, getAllPagesInSpace } from 'notion-utils'
 import { NotionAPI } from 'notion-client'
 import { NotionRenderer } from 'react-notion-x'
 
+const isDev = process.env.NODE_ENV === 'development' || !process.env.NODE_ENV
+
 const notion = new NotionAPI()
 
 export const getStaticProps = async (context) => {
@@ -20,33 +22,36 @@ export const getStaticProps = async (context) => {
 }
 
 export async function getStaticPaths() {
+  if (isDev) {
+    return {
+      paths: [],
+      fallback: true
+    }
+  }
+
+  const rootNotionPageId = '067dd719a912471ea9a3ac10710e7fdf'
+  const rootNotionSpaceId = 'fde5ac74-eea3-4527-8f00-4482710e1af3'
+
+  // This crawls all public pages starting from the given root page in order
+  // for next.js to pre-generate all pages via static site generation (SSG).
+  // This is a useful optimization but not necessary; you could just as easily
+  // set paths to an empty array to not pre-generate any pages at build time.
+  const pages = await getAllPagesInSpace(
+    rootNotionPageId,
+    rootNotionSpaceId,
+    notion.getPage.bind(notion),
+    {
+      traverseCollections: false
+    }
+  )
+
+  const paths = Object.keys(pages).map((pageId) => `/${pageId}`)
+
   return {
-    paths: [],
+    paths,
     fallback: true
   }
 }
-
-// export async function getStaticPaths() {
-//   const rootNotionPageId = '067dd719a912471ea9a3ac10710e7fdf'
-//   const rootNotionSpaceId = 'fde5ac74-eea3-4527-8f00-4482710e1af3'
-
-//   // This crawls all public pages starting from the given root page in order
-//   // for next.js to pre-generate all pages via static site generation (SSG).
-//   // This is a useful optimization but not necessary; you could just as easily
-//   // set paths to an empty array to not pre-generate any pages at build time.
-//   const pages = await getAllPagesInSpace(
-//     rootNotionPageId,
-//     rootNotionSpaceId,
-//     notion.getPage.bind(notion)
-//   )
-
-//   const paths = Object.keys(pages).map((pageId) => `/${pageId}`)
-
-//   return {
-//     paths,
-//     fallback: true
-//   }
-// }
 
 export default function NotionPage({ recordMap }) {
   if (!recordMap) {
