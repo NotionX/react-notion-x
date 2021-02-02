4fe6b07dcc0fd18ca14d6943c5130480f8034b11
Travis Fischer <fisch0920@gmail.com>
2021-02-01 23:40:47 -0500

    😀

--- packages/react-notion-x/src/third-party.tsx
+++ packages/react-notion-x/src/third-party.tsx
@@ -4,10 +4,20 @@ import { Page, Document } from 'react-pdf'
 import Equation from '@matejmazur/react-katex'
 import Modal from 'react-modal'
 
-const Pdf = ({ file, children, ...rest }) => (
-  <Document file={file} {...rest}>
-    <Page pageNumber={1} />
-  </Document>
-)
+const Pdf = ({ file, children, ...rest }) => {
+  const [numPages, setNumPages] = React.useState(null)
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages)
+  }
+
+  return (
+    <Document file={file} onLoadSuccess={onDocumentLoadSuccess} {...rest}>
+      {Array.from(new Array(numPages), (_, index) => (
+        <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+      ))}
+    </Document>
+  )
+}
 
 export { Tweet, Pdf, Equation, Modal }
