1054ee9b078d5dae41225c22a56421d3282aa578
Travis Fischer <fisch0920@gmail.com>
2021-02-03 20:15:56 -0500

    fix: differentiate btwn youtube embeds and video embeds

--- packages/notion-client/src/notion-api.test.ts
+++ packages/notion-client/src/notion-api.test.ts
@@ -6,6 +6,7 @@ const pageIdFixturesSuccess = [
   '067dd719-a912-471e-a9a3-ac10710e7fdf',
   '067dd719a912471ea9a3ac10710e7fdf',
   'https://www.notion.so/saasifysh/Embeds-5d4e290ca4604d8fb809af806a6c1749',
+  'https://www.notion.so/saasifysh/File-Uploads-34d650c65da34f888335dbd3ddd141dc',
   'Color-Rainbow-54bf56611797480c951e5c1f96cb06f2',
   'e68c18a461904eb5a2ddc3748e76b893',
   'https://www.notion.so/saasifysh/Saasify-Key-Takeaways-689a8abc1afa4699905aa2f2e585e208',
