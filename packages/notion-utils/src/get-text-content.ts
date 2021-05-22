40b00a781bab20442501d53e61a87dcf878fe001
Travis Fischer <fisch0920@gmail.com>
2021-05-21 22:34:04 -0400

    chore: fix ci

--- packages/notion-utils/src/get-text-content.ts
+++ packages/notion-utils/src/get-text-content.ts
@@ -10,7 +10,13 @@ export const getTextContent = (text?: types.Decoration[]): string => {
   if (!text) {
     return ''
   } else if (Array.isArray(text)) {
-    return text?.reduce((prev, current) => prev + ((current[0] !== '⁍' && current[0] !== '‣') ? current[0] : ''), '') ?? ''
+    return (
+      text?.reduce(
+        (prev, current) =>
+          prev + (current[0] !== '⁍' && current[0] !== '‣' ? current[0] : ''),
+        ''
+      ) ?? ''
+    )
   } else {
     return text
   }
