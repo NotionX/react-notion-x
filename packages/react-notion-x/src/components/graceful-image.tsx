cdde7d7f6068aaaa273b7a74a221f3b536e32aff
Callum McIntyre <callum.mcintyre@procensus.com>
2021-06-12 21:21:42 +0100

    Allow SSR for GracefulImage
    When rendered in a non-browser environment, fallback to <img> instead of using react-image
    
    Note that react-image deliberately does not SSR images: https://github.com/mbrevda/react-image/issues/206

--- packages/react-notion-x/src/components/graceful-image.tsx
+++ packages/react-notion-x/src/components/graceful-image.tsx
@@ -1,3 +1,12 @@
-import { Img } from 'react-image'
+import React from 'react'
+import { Img, ImgProps } from 'react-image'
+import { isBrowser } from '../utils'
 
-export const GracefulImage = Img
+export const GracefulImage = (props: ImgProps) => {
+  if(isBrowser) {
+    return <Img {...props} />
+  } else {
+    // @ts-ignore (must use the appropriate subset of props for <img> if using SSR)
+    return <img {...props} />
+  }
+}
