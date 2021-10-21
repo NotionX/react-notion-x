972017bc9c907d7011c8c85012cccc63d40fa47a
Travis Fischer <fisch0920@gmail.com>
2021-10-21 11:53:14 -0400

    feat: remove react-pdf

--- packages/react-notion-x/src/third-party.tsx
+++ packages/react-notion-x/src/third-party.tsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import { Page, Document } from 'react-pdf'
+// import React from 'react'
+// import { Page, Document } from 'react-pdf'
 import Equation from '@matejmazur/react-katex'
 import Modal from 'react-modal'
 
-const Pdf = ({ file, children, ...rest }) => {
-  const [numPages, setNumPages] = React.useState(null)
+// TODO: PDF support via "react-pdf" package has numerous troubles building
+// with next.js so I'm choosing to disable support for now.
 
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages)
-  }
+// const Pdf = ({ file, children, ...rest }) => {
+//   const [numPages, setNumPages] = React.useState(null)
 
-  return (
-    <Document file={file} onLoadSuccess={onDocumentLoadSuccess} {...rest}>
-      {Array.from(new Array(numPages), (_, index) => (
-        <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-      ))}
-    </Document>
-  )
-}
+//   function onDocumentLoadSuccess({ numPages }) {
+//     setNumPages(numPages)
+//   }
 
-export { Pdf, Equation, Modal }
+//   return (
+//     <Document file={file} onLoadSuccess={onDocumentLoadSuccess} {...rest}>
+//       {Array.from(new Array(numPages), (_, index) => (
+//         <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+//       ))}
+//     </Document>
+//   )
+// }
+
+export { Equation, Modal }
