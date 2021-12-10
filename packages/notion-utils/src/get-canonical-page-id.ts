aa903381a5ff282171c6640f286440443df44524
vv13 <zwhvv13@gmail.com>
2021-12-10 11:32:02 +0800

    feat: support for normalize chinese title

--- packages/notion-utils/src/get-canonical-page-id.ts
+++ packages/notion-utils/src/get-canonical-page-id.ts
@@ -33,7 +33,7 @@ export const getCanonicalPageId = (
 export const normalizeTitle = (title: string | null): string => {
   return (title || '')
     .replace(/ /g, '-')
-    .replace(/[^a-zA-Z0-9-]/g, '')
+    .replace(/[^a-zA-Z0-9-\u4e00-\u9fa5]/g, '')
     .replace(/--/g, '-')
     .replace(/-$/, '')
     .replace(/^-/, '')
