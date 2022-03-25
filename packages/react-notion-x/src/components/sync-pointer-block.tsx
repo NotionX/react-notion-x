fce35875b868c8bea9704606fea089746459c849
Travis Fischer <fisch0920@gmail.com>
2022-03-25 02:47:54 -0400

    fix microbundle

--- packages/react-notion-x/src/components/sync-pointer-block.tsx
+++ packages/react-notion-x/src/components/sync-pointer-block.tsx
@@ -1,28 +1,30 @@
+import React from 'react'
 import {
   SyncPointerBlock as SyncPointerBlockType,
   Block as BlockType
 } from 'notion-types'
-import React from 'react'
 
 import { NotionBlockRenderer } from '../renderer'
 
 export const SyncPointerBlock: React.FC<{
   block: BlockType
   level: number
-}> = (props) => {
-  const { block, level } = props
-
+}> = ({ block, level }) => {
   if (!block) {
     if (process.env.NODE_ENV !== 'production') {
-      console.warn('missing block', block.id)
+      console.warn('missing sync pointer block', block.id)
     }
+
     return null
   }
+
   const syncPointerBlock = block as SyncPointerBlockType
   const referencePointerId =
     syncPointerBlock?.format?.transclusion_reference_pointer?.id
 
-  if (!referencePointerId) return null
+  if (!referencePointerId) {
+    return null
+  }
 
   return (
     <NotionBlockRenderer
