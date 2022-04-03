7468a0e54ab51d9b8badca46b9c17f3072c675ae
Travis Fischer <fisch0920@gmail.com>
2022-04-03 17:18:15 -0400

    🚕

--- examples/full/lib/notion.ts
+++ examples/full/lib/notion.ts
@@ -15,6 +15,12 @@ const notion = useOfficialNotionAPI
   ? new NotionCompatAPI(new Client({ auth: process.env.NOTION_TOKEN }))
   : new NotionAPI()
 
+if (useOfficialNotionAPI) {
+  console.warn(
+    'Using the official Notion API. Note that many blocks only include partial support for formatting and layout. Use at your own risk.'
+  )
+}
+
 export async function getPage(pageId: string): Promise<ExtendedRecordMap> {
   const recordMap = await notion.getPage(pageId)
 
