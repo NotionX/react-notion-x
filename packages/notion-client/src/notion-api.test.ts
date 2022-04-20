1dda2dc8e9eb855c51e8cbe1be0c2519101d9253
Travis Fischer <fisch0920@gmail.com>
2022-04-19 21:16:35 -0400

    🎧

--- packages/notion-client/src/notion-api.test.ts
+++ packages/notion-client/src/notion-api.test.ts
@@ -26,6 +26,8 @@ const pageIdFixturesFailure = [
 
 for (const pageId of pageIdFixturesSuccess) {
   test(`NotionAPI.getPage success ${pageId}`, async (t) => {
+    t.timeout(60000) // one minute timeout
+
     const api = new NotionAPI()
     const page = await api.getPage(pageId)
 
