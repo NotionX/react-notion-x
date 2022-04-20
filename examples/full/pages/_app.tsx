cd6bc2a945f1ca2b089504f9feed0cd466e87b72
Travis Fischer <fisch0920@gmail.com>
2022-04-19 21:04:02 -0400

    feat: revert to official tweet embedding sdk

--- examples/full/pages/_app.tsx
+++ examples/full/pages/_app.tsx
@@ -11,9 +11,6 @@ import 'prismjs/themes/prism-tomorrow.css'
 // used for rendering equations (optional)
 import 'katex/dist/katex.min.css'
 
-// used for tweet embeds (optional)
-import 'react-static-tweets/styles.css'
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
