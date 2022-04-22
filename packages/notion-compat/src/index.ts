163f5383406ddb1836b6253a2473988fe2fa7ffa
Travis Fischer <fisch0920@gmail.com>
2022-04-22 13:11:14 -0400

    〽

--- packages/notion-compat/src/index.ts
+++ packages/notion-compat/src/index.ts
@@ -1,4 +1,6 @@
 export * from './notion-compat-api'
+export * from './convert-page'
 export * from './convert-block'
 export * from './convert-time'
+export * from './convert-color'
 export * from './convert-rich-text'
