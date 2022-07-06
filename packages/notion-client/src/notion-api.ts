065ebfa8ec6d139146ebe998a2fd7b93027cb7ca
Trang Le <lethutrang101@gmail.com>
2022-07-06 11:53:06 +0700

    fix: make collectionView parameter optional

--- packages/notion-client/src/notion-api.ts
+++ packages/notion-client/src/notion-api.ts
@@ -309,7 +309,7 @@ export class NotionAPI {
   public async getCollectionData(
     collectionId: string,
     collectionViewId: string,
-    collectionView: any,
+    collectionView?: any,
     {
       limit = 9999,
       searchQuery = '',
