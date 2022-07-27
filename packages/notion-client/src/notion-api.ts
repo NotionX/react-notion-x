1c6f6aaf9f0965834d5d69fa4027fb655bc989dc
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2022-07-27 06:26:18 +0530

    remove unused code

--- packages/notion-client/src/notion-api.ts
+++ packages/notion-client/src/notion-api.ts
@@ -227,10 +227,7 @@ export class NotionAPI {
           block.type === 'video' ||
           block.type === 'file')
       ) {
-        const source =
-          block.type === 'page'
-            ? block.format?.page_cover
-            : block.properties?.source?.[0]?.[0]
+        const source = block.properties?.source?.[0]?.[0]
         // console.log(block, source)
 
         if (source) {
