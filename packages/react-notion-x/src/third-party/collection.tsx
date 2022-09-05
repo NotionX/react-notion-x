f2ea40a6c7e855c787a7d71adfcd3c0beb76d44a
Utku Turunç <utku@turunc.me>
2022-09-05 18:04:26 +0300

    fallback to collection view name if collection name is empty

--- packages/react-notion-x/src/third-party/collection.tsx
+++ packages/react-notion-x/src/third-party/collection.tsx
@@ -176,7 +176,8 @@ const CollectionViewBlock: React.FC<{
     return null
   }
 
-  const title = getTextContent(collection.name).trim()
+  const title =
+    getTextContent(collection.name).trim() || collectionView.name.trim()
   if (collection.icon) {
     block.format = {
       ...block.format,
