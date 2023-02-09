0ec41f1453bcbe57da8581635bddd1e010555402
mosugi <mosuging@gmail.com>
2023-02-09 13:22:41 +0900

    feat: support for normalize japanese hiragana and katakana title

--- packages/notion-utils/src/normalize-title.ts
+++ packages/notion-utils/src/normalize-title.ts
@@ -1,7 +1,7 @@
 export const normalizeTitle = (title?: string | null): string => {
   return (title || '')
     .replace(/ /g, '-')
-    .replace(/[^a-zA-Z0-9-\u4e00-\u9fa5]/g, '')
+    .replace(/[^a-zA-Z0-9-\u4e00-\u9fa5\u3041-\u3096\u30A1-\u30FA]/g, '')
     .replace(/--/g, '-')
     .replace(/-$/, '')
     .replace(/^-/, '')
