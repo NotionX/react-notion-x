2bd81e1fe75170e830d23ce26f4fb8a4e0e97163
Paul Gerlich <paul@height.app>
2023-02-24 12:48:56 -0600

    Render user mentions as text

--- packages/react-notion-x/src/components/text.tsx
+++ packages/react-notion-x/src/components/text.tsx
@@ -203,24 +203,8 @@ export const Text: React.FC<{
               }
 
               case 'u': {
-                const userId = decorator[1]
-                const user = recordMap.notion_user[userId]?.value
-
-                if (!user) {
-                  console.log('missing user', userId)
-                  return null
-                }
-
-                const name = [user.given_name, user.family_name]
-                  .filter(Boolean)
-                  .join(' ')
-
                 return (
-                  <GracefulImage
-                    className='notion-user'
-                    src={mapImageUrl(user.profile_photo, block)}
-                    alt={name}
-                  />
+                  {text}
                 )
               }
 
