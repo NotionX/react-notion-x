c27720223e9fea3ce765ec49c9bc28ea891fc535
Francisco Moretti <franciscoemoretti@gmail.com>
2023-03-29 13:36:51 +0100

    Migrated basics from yarn + lerna to turbo + pnpm

---
+++ packages/notion-types/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  outDir: 'build',
+  target: 'es2015',
+  platform: 'browser',
+  format: ['esm'],
+  splitting: false,
+  sourcemap: true,
+  minify: true,
+  shims: false
+})
