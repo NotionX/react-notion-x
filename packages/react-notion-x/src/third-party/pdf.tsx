29111e50f150bb6c6ffc11228499f880ab66edde
worgho2 <otavio.baziewicz.filho@gmail.com>
2024-12-14 01:40:30 -0300

    fix: Update react-pdf worker cdn src

--- packages/react-notion-x/src/third-party/pdf.tsx
+++ packages/react-notion-x/src/third-party/pdf.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Document, Page, pdfjs } from 'react-pdf'
 
 // ensure pdfjs can find its worker script regardless of how react-notion-x is bundled
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.mjs`
 
 export function Pdf({ file, ...rest }: { file: string }) {
   const [numPages, setNumPages] = React.useState<number>(0)
