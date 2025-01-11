333c64d969337d28c475a2db10104974f40d616e
Carson <carson@voiceqube.com>
2025-01-12 01:19:26 +0530

    simplyfied and faster lazy loading

--- packages/react-notion-x/src/components/lazy-image.tsx
+++ packages/react-notion-x/src/components/lazy-image.tsx
@@ -1,110 +1,27 @@
-// eslint-disable-next-line simple-import-sort/imports
-import * as React from 'react'
-
-import LazyLoad from 'react-lazyload' // Import react-lazyload
-import { cs } from '../utils'
-import { normalizeUrl } from 'notion-utils'
-import { useNotionContext } from '../context'
-
-/**
- * Progressive, lazy images modeled after Medium's LQIP technique.
- */
 export function LazyImage({
   src,
   alt,
   className,
   style,
-  zoomable = false,
-  priority = false,
   height,
-  ...rest
+  priority = false
 }: {
   src?: string
   alt?: string
   className?: string
   style?: React.CSSProperties
   height?: number
-  zoomable?: boolean
   priority?: boolean
 }) {
-  const { recordMap, zoom, previewImages, components } = useNotionContext()
-  const zoomRef = React.useRef(zoom ? zoom.clone() : null)
-  const previewImage = previewImages
-    ? (recordMap?.preview_images?.[src!] ??
-      recordMap?.preview_images?.[normalizeUrl(src)])
-    : null
-
-  const onLoad = React.useCallback(
-    (e: any) => {
-      if (zoomable && (e.target.src || e.target.srcset)) {
-        if (zoomRef.current) {
-          ;(zoomRef.current as any).attach(e.target)
-        }
-      }
-    },
-    [zoomRef, zoomable]
+  return (
+    <img
+      src={src}
+      alt={alt}
+      className={className}
+      style={style}
+      height={height}
+      loading={priority ? 'eager' : 'lazy'} // Native lazy loading
+      decoding='async' // For better performance and asynchronous decoding
+    />
   )
-
-  const attachZoom = React.useCallback(
-    (image: any) => {
-      if (zoomRef.current && image) {
-        ;(zoomRef.current as any).attach(image)
-      }
-    },
-    [zoomRef]
-  )
-
-  const attachZoomRef = React.useMemo(
-    () => (zoomable ? attachZoom : undefined),
-    [zoomable, attachZoom]
-  )
-
-  if (previewImage) {
-    // const aspectRatio = previewImage.originalHeight / previewImage.originalWidth
-
-    if (components.Image) {
-      return (
-        <components.Image
-          src={src}
-          alt={alt}
-          style={style}
-          className={className}
-          width={previewImage.originalWidth}
-          height={previewImage.originalHeight}
-          blurDataURL={previewImage.dataURIBase64}
-          placeholder='blur'
-          priority={priority}
-          onLoad={onLoad}
-        />
-      )
-    }
-
-    return (
-      <LazyLoad height={height || 'auto'} offset={100} once>
-        <img
-          className={cs('lazy-image-wrapper', className)}
-          src={src}
-          alt={alt}
-          ref={attachZoomRef}
-          style={{ ...style, width: '100%', height: 'auto' }}
-          decoding='async'
-          loading='lazy'
-        />
-      </LazyLoad>
-    )
-  } else {
-    // Default image element
-    return (
-      <img
-        className={className}
-        style={style}
-        src={src}
-        alt={alt}
-        ref={attachZoomRef}
-        loading='lazy'
-        decoding='async'
-        {...rest}
-      />
-    )
-  }
 }
