a49b5cfbe2b64f91ef256b6d714fa217ea763007
Travis Fischer <fisch0920@gmail.com>
2026-03-18 21:33:10 +0700

    💉

--- packages/notion-to-md/src/index.test.ts
+++ packages/notion-to-md/src/index.test.ts
@@ -1,4 +1,4 @@
-import { writeFile } from 'node:fs/promises'
+import { mkdir, writeFile } from 'node:fs/promises'
 
 import { NotionAPI } from 'notion-client'
 import { expect, test } from 'vitest'
@@ -6,9 +6,9 @@ import { expect, test } from 'vitest'
 import { notionPageToMarkdown } from '.'
 
 const pageIdFixtures = [
-  // '067dd719a912471ea9a3ac10710e7fdf'
-  'de14421f13914ac7b528fa2e31eb1455' // lists
-  // '0be6efce9daf42688f65c76b89f8eb27'
+  '067dd719a912471ea9a3ac10710e7fdf',
+  'de14421f13914ac7b528fa2e31eb1455', // lists
+  '0be6efce9daf42688f65c76b89f8eb27'
 ]
 
 for (const id of pageIdFixtures) {
@@ -23,7 +23,8 @@ for (const id of pageIdFixtures) {
       const markdown = notionPageToMarkdown(recordMap)
 
       console.log(`${id}.md\n${markdown}`)
-      await writeFile(`${id}.md`, markdown)
+      await mkdir('out', { recursive: true })
+      await writeFile(`out/${id}.md`, markdown)
 
       expect(markdown).toBeTruthy()
       expect(markdown.trim()).toMatchSnapshot()
